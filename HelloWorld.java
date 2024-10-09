db7207a5e1e871e08a7401bb22521f9f93bb9a7b
ftrentinaglia <137480708+Ftrentinaglia@users.noreply.github.com>
2024-10-09 11:26:47 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld{
+  
   public static void main (string []args)
-  {
-    System.out.println("Hello Ftrentinaglia");
-  }
+    {
+      //diese Methode gibt Hello Ftrentinaglia aus
+      System.out.println("Hello Ftrentinaglia");
+    }
 }
   
