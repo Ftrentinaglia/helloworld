668f074d573515b7de365fd40f64935855220d4d
ftrentinaglia <137480708+Ftrentinaglia@users.noreply.github.com>
2024-10-11 14:55:42 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld{
   public static void main (string []args)
     {
       //diese Methode gibt Hello Ftrentinaglia aus
-      System.out.println("Hello Ftrentinaglia");
+      System.out.println("Hello Ftrentinaglia!");
     }
 }
   
